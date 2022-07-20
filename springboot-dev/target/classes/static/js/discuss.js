eeaa4c4f35b6dc5062ed867d5a675cc72c34fd87
HEYHOME <1414626899@qq.com>
2022-07-20 16:23:10 +0800

    three commit

--- springboot-dev/target/classes/static/js/discuss.js
+++ springboot-dev/target/classes/static/js/discuss.js
@@ -1,7 +1,13 @@
-function like(btn, entityType, entityId,entityUserId,postId) {
+$(function () {
+    $("#topBtn").click(setTop);
+    $("#wonderfulBtn").click(setWonderful);
+    $("#deleteBtn").click(setDelete);
+})
+
+function like(btn, entityType, entityId, entityUserId, postId) {
     $.post(
         CONTEXT_PATH + "/like",
-        {"entityType": entityType, "entityId": entityId,"entityUserId":entityUserId,"postId":postId},
+        {"entityType": entityType, "entityId": entityId, "entityUserId": entityUserId, "postId": postId},
         function (data) {
             data = $.parseJSON(data);
             if (data.code == 0) {
@@ -12,4 +18,55 @@ function like(btn, entityType, entityId,entityUserId,postId) {
             }
         }
     )
+}
+
+// 置顶
+function setTop() {
+    $.ajax({
+        type: "post",
+        url: CONTEXT_PATH + "/discuss/top",
+        dataType: "json",
+        data: {"id": $("#postId").val()},
+        success: function (data) {
+            if (data.code == 0) {
+                $("#topBtn").attr("disabled", "disabled");
+            } else {
+                alert(data.msg);
+            }
+        }
+    })
+}
+
+// 加精
+function setWonderful() {
+    $.ajax({
+        type: "post",
+        url: CONTEXT_PATH + "/discuss/wonderful",
+        dataType: "json",
+        data: {"id": $("#postId").val()},
+        success: function (data) {
+            if (data.code == 0) {
+                $("#wonderfulBtn").attr("disabled", "disabled");
+            } else {
+                alert(data.msg);
+            }
+        }
+    })
+}
+
+// 删除
+function setDelete() {
+    $.ajax({
+        type: "post",
+        url: CONTEXT_PATH + "/discuss/delete",
+        dataType: "json",
+        data: {"id": $("#postId").val()},
+        success: function (data) {
+            if (data.code == 0) {
+                location.href = CONTEXT_PATH + "/index";
+            } else {
+                alert(data.msg);
+            }
+        }
+    })
 }
